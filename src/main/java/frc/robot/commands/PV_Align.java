e3ed3c48082adfbd69cc0382ec15adde68f676f4
PMSCS50 <64873878+PMSCS50@users.noreply.github.com>
2026-01-29 16:37:47 -0600

    Update PV_Align.java

--- src/main/java/frc/robot/commands/PV_Align.java
+++ src/main/java/frc/robot/commands/PV_Align.java
@@ -1,46 +1,115 @@
 package frc.robot.commands;
 
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.VisionSubsystem;
+
 public class PV_Align extends Command {
-    private PhotonCamera camera;
-    public static final AprilTagFieldLayout aprilTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
-    public PV_Align(PhotonCamera camera) {
-        this.camera = camera;
+
+    private final PIDController xController =
+        new PIDController(0.4, 0, 0);   // forward
+    private final PIDController yController =
+        new PIDController(0.4, 0, 0);   // sideways
+    private final PIDController rotController =
+        new PIDController(0.4, 0, 0);   // yaw
+
+    private final CommandSwerveDrivetrain drivetrain;
+    private final VisionSubsystem vision;
+
+
+    private final SwerveRequest.RobotCentric drive =
+            new SwerveRequest.RobotCentric();
+
+    private Timer dontSeeTagTimer, stopTimer, idleTimer;
+
+    private static final int TARGET_TAG_ID = 18;
+
+     
+
+
+    public PV_Align(CommandSwerveDrivetrain drivetrain, VisionSubsystem vision) {
+        this.drivetrain = drivetrain;
+        this.vision = vision;
+        xController.setSetpoint(-.25);
+        yController.setSetpoint(0.0);
+            rotController.setSetpoint(-1.0);
+
+        xController.setTolerance(0.02);
+        yController.setTolerance(0.02);
+        rotController.setTolerance(0.25);
+
+        addRequirements(drivetrain);
     }
+
+    @Override
+    public void initialize() {
+        xController.reset();
+        yController.reset();
+        rotController.reset();
+
+        stopTimer = new Timer();
+        stopTimer.start();
+
+        dontSeeTagTimer = new Timer();
+        dontSeeTagTimer.start();
+
+        idleTimer = new Timer();
+        idleTimer.start();
+    }
+
+    @Override
     public void execute() {
-        
-    }
-    public PhotonPipelineResult getLatestResult() {
-        return camera.getLatestResult();
-    }
-    public void align() {
-        var result = camera.getLatestResult();
-        boolean hasTargets = result.hasTargets();
-        
-        if (hasTargets) {
-            PhotonTrackedTarget target = result.getBestTarget();
-            int targetID = target.getFiducialId();
-            double poseAmbiguity = target.getPoseAmbiguity();
-            Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-            Transform3d alternateCameraToTarget = target.getAlternateCameraToTarget();
-            Pose3d robotPose;
-            Pose3d targetPose;
-            if (aprilTagLayout.getTagPose(target.getFiducialId()).isPresent()) {
-                robotPose = PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(), aprilTagLayout.getTagPose(target.getFiducialId()).get(), Constants.VisionConstants.robotToCamera);
-                targetPose = aprilTagLayout.getTagPose(target.getFiducialId()).get();
+
+        if (vision.hasTargets() && vision.getTargetID() == TARGET_TAG_ID) {
+
+            dontSeeTagTimer.reset();
+
+           
+            SmartDashboard.putNumber("x", vision.getX());
+            SmartDashboard.putNumber("y", vision.getY());
+            SmartDashboard.putNumber("rot", vision.getRot());
+
+
+
+
+            
+            double xOut   = xController.calculate(vision.getX());
+            double yOut   = -yController.calculate(vision.getY());
+            double rotOut = rotController.calculate(vision.getRot());
+
+            drivetrain.setControl(
+                drive.withVelocityX(xOut)
+                     .withVelocityY(yOut)
+                     .withRotationalRate(rotOut)
+            );
+            if (!xController.atSetpoint()
+             || !yController.atSetpoint()
+             || !rotController.atSetpoint()) {
+                stopTimer.reset();
             }
-            //double distanceToTarget = PhotonUtils.getDistanceToPose(robotPose, targetPose);
         }
     }
-}
 
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.setControl(
+            drive.withVelocityX(0)
+                 .withVelocityY(0)
+                 .withRotationalRate(0)
+        );
+    }
+
+    @Override
+    public boolean isFinished() {
+        return dontSeeTagTimer.hasElapsed(Constants.DONT_SEE_TAG_WAIT_TIME)
+            || stopTimer.hasElapsed(Constants.POSE_VALIDATION_TIME)
+            || idleTimer.hasElapsed(5);
+    }
+}
