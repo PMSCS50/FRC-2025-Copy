7a9ea38e59612ad1d1f3355712956c04b52644f9
Future_Gohan <s1841390@online.houstonisd.org>
2026-02-17 18:23:12 -0600

    Revamped Shooter.java, replaced shooterMotor with TalonFX

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,10 +4,16 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import frc.robot.ShooterConstants;
+import frc.robot.Constants.ShooterConstants;
 
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -26,26 +32,26 @@ Finish convertToRPM();
 
 public class Shooter extends SubsystemBase {
     // Configuration for the shooter motor
-    private final SparkMaxConfig shooterMotorConfig = new SparkMaxConfig();
+    private final TalonFXConfiguration shooterMotorConfig = new TalonFXConfiguration();
     private final SparkMaxConfig kickerMotorConfig = new SparkMaxConfig();
 
-    final SparkMax shooterMotor = new SparkMax(ShooterConstants.shooterMotorCanId, MotorType.kBrushless);
+    final TalonFX shooterMotor = new TalonFX(ShooterConstants.shooterMotorCanId);
     final SparkMax kickerMotor = new SparkMax(ShooterConstants.kickerMotorCanId, MotorType.kBrushless);
     
+    private final VelocityVoltage velocityRequest = new VelocityVoltage(0);
+
+
 
     // Current commanded output (interpreted as percent output by SparkMax.set)
     private double velocity = 0.0;
     public double shooterAngle = 70.0; //shooter angle
     private double shooterHeight = 0.508; //How high the shooter is from the ground (meters)
-    private double voltage = 10; //temporary value
 
     public Shooter() {
-        shooterMotorConfig
-            // .inverted(true)
-            .idleMode(IdleMode.kCoast)
-            .smartCurrentLimit(20);
+        //TalonFX shooterMotorConfig
+        configureShooterMotor();
 
-        shooterMotor.configure(shooterMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        shooterMotor.getConfigurator().apply(shooterMotorConfig);
 
         kickerMotorConfig
             // .inverted(true)
@@ -53,6 +59,16 @@ public class Shooter extends SubsystemBase {
             .smartCurrentLimit(20);
 
         kickerMotor.configure(shooterMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    private void configureShooterMotor() {
+        shooterMotorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        shooterMotorConfig.CurrentLimits.SupplyCurrentLimit = 20;
+        shooterMotorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+        shooterMotorconfig.Slot0.kP = 8.3; // Just a guess, do later
+        shooterMotorconfig.Slot0.kI = 0;
+        shooterMotorconfig.Slot0.kD = 0;
     }
 
     @Override
@@ -65,20 +81,14 @@ public class Shooter extends SubsystemBase {
         double y = 1.8288 - shooterHeight; // y distance from shooter to hub. May have to change later
         double phi = Math.toRadians(shooterAngle);
         double v = Math.sqrt((9.807 * x * x) / (2 * Math.cos(phi) * Math.cos(phi) * (x*tan(phi) + shooterHeight - y)));       
-        return v;
+        double kp = 1.1;
+        return kp * v; //Fuck air resistance
     }
 
-    public void setVelocityFromDistance(double x) {
-        this.setVelocity(velocityFromDistance(x));
-    }
-    
-    public void startShooting() {
-        shooterMotor.getClosedLoopController().setReference(convertToRPM(velocity),ControlType.kVelocity);
-    }
 
-    public void startShootingAtVelocity(double newVelocity) {
+    public void setVelocityTo(double newVelocity) {
         velocity = newVelocity;
-        shooterMotor.getClosedLoopController().setReference(convertToRPM(velocity),ControlType.kVelocity);
+        shooterMotor.setControl(velocityRequest.withVelocity(convertToRPM(rpm / 60.0)));
     }
 
     
@@ -86,7 +96,7 @@ public class Shooter extends SubsystemBase {
     private double convertToRPM(double velocity) {
 
         double wheelRadius = 0.0508;
-        double kp = 1.3; //extra constant to try and account for energy loss
+        double kp = 1.1; //extra constant to try and account for energy loss
         
         double wheelRPM = kp * (velocity * 60.0) / (2.0 * Math.PI * wheelRadius);
 
@@ -94,28 +104,11 @@ public class Shooter extends SubsystemBase {
 
     }
 
-    // Hopefully this will work since SparkMax and Kraken take power, not rpm nor velocity
-    private double powerFromRPM(double rpm, double voltage) {
-        double maxRPM = 5640; //replace with v_f
-        double i_0 = 2.7; //replace with i_0
-        double i_s = 131; //replace with i_s
-        double t_s = 2.41; //replace with t_s (ts pmo)
-
-        double torque = t_s * (1 - rpm/maxRPM);
-        double current = ((1 - rpm/maxRPM) * (i_s - i_0) + i_0)/100;
-        double power = torque * (rpm * 2*Math.PI/60)/100;
-        double efficiency = power / (voltage * current);
-
-        power /= efficiency;
-        power /= 1108; //returns percentage 
-        return power;
-    }
-
-
     /** Stop the shooter. */
     public void stop() {
-        this.setShootingVelocity(0.0);
+        this.setVelocityTo(0.0);
     }
+
 
     public double getVelocity() {
         return velocity;
